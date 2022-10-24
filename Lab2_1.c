9da8c2a7d288754e95c08e06511d13d4b5a5da15
mdmY <114355088+RE22GDV@users.noreply.github.com>
2022-10-24 16:09:42 +0300

    Update Lab2_1.c

--- Lab2_1.c
+++ Lab2_1.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-double uravnenie1(double x){return -x;}
-double uravnenie2(double x){return x;}
-double uravnenie3(double x){return log10(x)+1;}
-double uravnenie_default(){ return 0;}
+double mathematical_expression1(double x){return -x;}
+double mathematical_expression2(double x){return x;}
+double mathematical_expression3(double x){return log10(x)+1;}
+double mathematical_expression_default(){ return 0;}
 
 
 #pragma clang diagnostic push
@@ -14,9 +14,9 @@ int main() {
     double x;
     printf("Enter x: ");
     scanf("%lf", &x);
-    if (-100 <= x && x < -50)printf("y = %.2lf\n", uravnenie1(x));
-    else if (50 <= x && x < 100) { printf("y = %.2lf\n", uravnenie2(x)); }
-    else if (200 <= x && x < 1000) { printf("y = %.2lf\n", uravnenie3(x)); }
-    else { printf("y = %d\n", uravnenie_default()); }
+    if (-100 <= x && x < -50)printf("y = %.2lf\n", mathematical_expression1(x));
+    else if (50 <= x && x < 100) { printf("y = %.2lf\n", mathematical_expression2(x)); }
+    else if (200 <= x && x < 1000) { printf("y = %.2lf\n", mathematical_expression3(x)); }
+    else { printf("y = %d\n", mathematical_expression_default()); }
 }
 
