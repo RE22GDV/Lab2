3e0736ff4ddc234ae8b47c300c0749f01ca14b57
mdmY <114355088+RE22GDV@users.noreply.github.com>
2022-10-24 16:30:30 +0300

    Update Lab2_1.c

--- Lab2_1.c
+++ Lab2_1.c
@@ -2,21 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 
-double mathematical_expression1(double x){return -x;}
-double mathematical_expression2(double x){return x;}
-double mathematical_expression3(double x){return log10(x)+1;}
-double mathematical_expression_default(){ return 0;}
+void mathematical_expression1(double x) {printf("y = %.2lf\n", -x);}
+void mathematical_expression2(double x) {printf("y = %.2lf\n", x);}
+void mathematical_expression3(double x) {printf("y = %.2lf\n", log10(x)+1);}
+void mathematical_expression_default() {printf("y = %.2f\n", 0.00);}
 
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wformat"
 int main() {
     double x;
     printf("Enter x: ");
     scanf("%lf", &x);
-    if (-100 <= x && x < -50)printf("y = %.2lf\n", mathematical_expression1(x));
-    else if (50 <= x && x < 100) { printf("y = %.2lf\n", mathematical_expression2(x)); }
-    else if (200 <= x && x < 1000) { printf("y = %.2lf\n", mathematical_expression3(x)); }
-    else { printf("y = %d\n", mathematical_expression_default()); }
+    if (-100 <= x && x < -50) {mathematical_expression1(x);}
+    else if (50 <= x && x < 100) {mathematical_expression2(x);}
+    else if (200 <= x && x < 1000) {mathematical_expression3(x);}
+    else {mathematical_expression_default();}
 }
-
